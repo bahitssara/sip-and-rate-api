3fc85859489a1aba0ceedc4b74b4587263b69196
Sara Mayberry <saraamayberry@yahoo.com>
2019-06-01 16:40:47 -0600

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sip & Rate Server/API
+
+## Scripts
+
+Start the application `npm start`
+
+Start nodemon for the application `npm run dev`
+
+Run the tests `npm test`
+
---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app')
+const { PORT } = require('./config')
+app.listen(PORT, () => {
+    console.log(`Sever is listening at http://localhost:${PORT}`)
+})
