2a9eedbfcd4f827e1df847deac63517a4aaece5a
Sara Mayberry <saraamayberry@yahoo.com>
2019-06-10 11:24:54 -0600

    middleware

--- src/Auth/auth-router.js
+++ src/Auth/auth-router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const AuthService= require('./auth-service')
+const requireAuth = require('../middleware/basic-auth')
 
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
@@ -51,6 +52,7 @@ authRouter
                 .catch(next)
         
           })
+
         
 
 
