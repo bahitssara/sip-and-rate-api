7ca8e6fa7ac912d521f1013bf5aa666101de612e
Sara Mayberry <saraamayberry@yahoo.com>
2019-07-01 12:59:05 -0600

    minor

--- src/Auth/auth-router.js
+++ src/Auth/auth-router.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const AuthService = require('./auth-service')
-const { requireAuth } = require('../middleware/jwt-auth') 
-
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
 
